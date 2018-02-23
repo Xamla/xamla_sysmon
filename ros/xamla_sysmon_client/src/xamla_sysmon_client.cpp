d41a1efe2eec3f690cb5b01b34dec41fab75e2eb
Anas I. Gouda <anas.ibrahim006@gmail.com>
2018-02-23 16:26:24 +0100

    deleted unnecessary vars - printed infos

--- ros/xamla_sysmon_client/src/xamla_sysmon_client.cpp
+++ ros/xamla_sysmon_client/src/xamla_sysmon_client.cpp
@@ -5,42 +5,34 @@ xamla_sysmon_client::xamla_sysmon_client()
 }
 
 void xamla_sysmon_client::start(ros::NodeHandle &node, int freq) {
+  //TODO check if already called
   this->node= node;
 
   if(freq > 0)
     this->freq = freq;
   else
-    this->freq = freq; //TODO print warn
+  {
+    this->freq = 10;
+    ROS_WARN("heatbeat freq cannot be 0 or less; default to 10");
+  }
+  ROS_INFO("[Heartbeat] publishing frequency: %d", this->freq);
 
-  this->last_publish_time = ros::Time::now();
-  this->status = GO;
-  ROS_INFO("[Heartbeat] publishing frequency: %d", freq);
-  //first message
-  this->heartbeat_msg.header.stamp = this->last_publish_time;
-  //this->heartbeat_msg.header.seq = 1;
-  this->heartbeat_msg.status = this->status;
-  this->details = "First heartbeart";
-  this->heartbeat_msg.details = this->details;
-
-  ROS_INFO("before thread");
+  this->heartbeat_msg.header.stamp = ros::Time::now();
+  this->heartbeat_msg.status = GO;
+  this->heartbeat_msg.details = "Node starting";
 
   //openning new thread to publish state
   this->pub_thread = std::thread(&xamla_sysmon_client::publish_status, this);
-
-  ROS_INFO("after thread");
-
-  //while(ros::ok()); //TODO remove this and keep thread alive without it
 }
 
 void xamla_sysmon_client::publish_status()
 {
+  //TODO add mutex to guard thread
   //ros publisher
   ROS_INFO("starting heartbeat publishing loop");
-  //TODO add namespace or node name before topic name
-  ros::Publisher status_pub = this->node.advertise<xamla_sysmon_msgs::HeartBeat>("heartbeat", 100);
+  ros::Publisher status_pub = this->node.advertise<xamla_sysmon_msgs::HeartBeat>(ros::this_node::getName()+"/heartbeat", 100);
   ros::Rate loop_rate(this->freq);
-  this->heartbeat_msg.status = this->status;
-  this->heartbeat_msg.details = this->details;
+
   while(ros::ok())
   {
     status_pub.publish(this->heartbeat_msg);
@@ -52,13 +44,12 @@ void xamla_sysmon_client::publish_status()
 
 void xamla_sysmon_client::shutdown()
 {
-  //TODO kill the thread here
+  //TODO shutdown the thread here
 }
 
 int32_t xamla_sysmon_client::updateStatus(heartbeat_status new_status, std::string details)
 {
   //TODO check if status is null or change it to type safe
-  //ROS_INFO("update status called");
-  this->status = new_status;
-  this->details = details;
+  this->heartbeat_msg.status = new_status;
+  this->heartbeat_msg.details = details;
 }
