92e951f5eafc87f9efce8c4333fa6aac578e2bb7
Manuel Volk <manuel.volk@xamla.com>
2018-05-23 16:36:05 +0200

    remove C dependencies from lua code

--- lua/status.lua
+++ lua/status.lua
@@ -1,4 +1,3 @@
-local ffi = require 'ffi'
 local torch = require 'torch'
 
 local xamla_sysmon = require 'xamla_sysmon.env'
@@ -6,7 +5,30 @@ local xamla_sysmon = require 'xamla_sysmon.env'
 
 local Statuscodes = torch.class('xamla_sysmon.status', xamla_sysmon)
 
+local StringToInt = {}
+StringToInt["GO"] = 0
+StringToInt["STARTING"] = 100
+StringToInt["INTERNAL_ERROR"] = 200
+StringToInt["EMERGENCY_STOP"] = 300
+StringToInt["SECONDARY_ERROR"] = 400
+
+local StringToDetails = {}
+StringToDetails["GO"] = "Node is GO."
+StringToDetails["STARTING"] = "Node is starting up."
+StringToDetails["INTERNAL_ERROR"] = "Node has an internal error or timed out."
+StringToDetails["EMERGENCY_STOP"] = "Node is in emergency stop."
+StringToDetails["SECONDARY_ERROR"] = "This node is GO, but another node is faulty."
+
 function Statuscodes:__init()
+
+setDefault (StringToInt, -1)
+setDefault (StringToDetails, "Unknown status code")
+
+end
+
+local function setDefault (t, d)
+  local mt = {__index = function () return d end}
+  setmetatable(t, mt)
 end
 
 function Statuscodes:generateMessageText(status_code)
@@ -14,5 +36,5 @@ function Statuscodes:generateMessageText(status_code)
 end
 
 function Statuscodes:getStatusCode(status_string)
-  return xamla_sysmon.lib.getStatusCode(status_string)
+  return StringToInt[status_string]
 end
