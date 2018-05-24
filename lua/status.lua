1b4df8fdd90ed77cd3c2d719a358694fc0fa32d6
Manuel Volk <manuel.volk@xamla.com>
2018-05-24 14:06:14 +0200

    remove all not necessary files and rework the status tables

--- lua/status.lua
+++ lua/status.lua
@@ -5,36 +5,43 @@ local xamla_sysmon = require 'xamla_sysmon.env'
 
 local Statuscodes = torch.class('xamla_sysmon.status', xamla_sysmon)
 
-local StringToInt = {}
-StringToInt["GO"] = 0
-StringToInt["STARTING"] = 100
-StringToInt["INTERNAL_ERROR"] = 200
-StringToInt["EMERGENCY_STOP"] = 300
-StringToInt["SECONDARY_ERROR"] = 400
-
-local IntToDetails = {}
-IntToDetails["0"] = "Node is GO."
-IntToDetails["100"] = "Node is starting up."
-IntToDetails["200"] = "Node has an internal error or timed out."
-IntToDetails["300"] = "Node is in emergency stop."
-IntToDetails["400"] = "This node is GO, but another node is faulty."
-
-function Statuscodes:__init()
-
-setDefault (StringToInt, -1)
-setDefault (IntToDetails, "Unknown status code")
-
-end
-
 local function setDefault (t, d)
   local mt = {__index = function () return d end}
   setmetatable(t, mt)
 end
 
+local SysmonStatusCodes = {
+ GO = 0,
+ STARTING = 100,
+ INTERNAL_ERROR = 200,
+ EMERGENCY_STOP = 300,
+ SECONDARY_ERROR = 400,
+ [0] = "GO",
+ [100] = "STARTING",
+ [200] = "INTERNAL_ERROR",
+ [300] = "EMERGENCY_STOP",
+ [400] = "SECONDARY_ERROR"
+}
+
+local StatusCodesToDetails = {
+ GO = "Node is GO.",
+ STARTING = "Node is starting up.",
+ INTERNAL_ERROR = "Node has an internal error or timed out.",
+ EMERGENCY_STOP = "Node is in emergency stop.",
+ SECONDARY_ERROR = "This node is GO, but another node is faulty."
+}
+
+setDefault (SysmonStatusCodes, "INVALID")
+setDefault (StatusCodesToDetails, "Unknown status code")
+
+function Statuscodes:__init()
+
+end
+
 function Statuscodes:generateMessageText(status_code)
-  return IntToDetails[tostring(status_code)]
+  return StatusCodesToDetails[SysmonStatusCodes[status_code]]
 end
 
 function Statuscodes:getStatusCode(status_string)
-  return StringToInt[status_string]
+  return SysmonStatusCodes[status_string]
 end
