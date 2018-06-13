5bae6a721c3afeadefda406b153097c9a724834b
Anas I. Gouda <anas.ibrahim006@gmail.com>
2018-06-13 16:32:26 +0200

    python sysmon: changed wrong timeout condition and types

--- ros/xamla_sysmon_python/scripts/xamla_sysmon_python/heartbeat.py
+++ ros/xamla_sysmon_python/scripts/xamla_sysmon_python/heartbeat.py
@@ -16,46 +16,48 @@ class HeartbeatCode(IntEnum):
 class HeartbeatClient:
     def __init__(self):
         self.heartbeat_publisher = None
-        self.frequency = 10
         self.stop_heartbeat = False
         self.mutex = Lock()
         self.msg = HeartBeat()
 
     def start(self, frequency):
-        self.frequency = frequency
+        self.frequency = float(frequency)
         self.rate = rospy.Rate(frequency)
+        self.timeout = float(1/self.frequency)
         self.heartbeat_publisher = rospy.Publisher(rospy.get_name() + '/heartbeat', HeartBeat, queue_size=10)
         self.msg.status = HeartbeatCode.STARTING
         self.msg.details = "STARTING"
         self.msg.header.stamp = rospy.Time.now()
         thread.start_new_thread(self.pub_thread, ())
-        rospy.loginfo("[Heartbeat] publishing frequency: %d", self.frequency )
+        rospy.loginfo("[Heartbeat] publishing frequency: %f", self.frequency )
     
     def update_status(self, new_status, details):
         if self.heartbeat_publisher == None:
             rospy.logerr("Cannot publish on an uninitialized/stopped heartbeat topic")
             return
-        self.msg.header.stamp = rospy.Time.now()
         if new_status != None:
             self.mutex.acquire()
             self.msg.status = new_status
             self.msg.details = details
+            self.msg.header.stamp = rospy.Time.now()
             self.mutex.release()
         else:
+            self.mutex.acquire()
             self.msg.status = HeartbeatCode.INVALID
             self.msg.details = "Invalid internal node state"
+            self.msg.header.stamp = rospy.Time.now()
+            self.mutex.release()
         
     def pub_thread(self):
         while not rospy.is_shutdown() and self.stop_heartbeat != True:
             self.mutex.acquire()
-            if rospy.Time.now().to_sec() - rospy.Time.now().to_sec() >= 1/self.frequency:
+            if rospy.Time.now().to_sec() - self.msg.header.stamp.to_sec() >= self.timeout:
                 self.msg.status = HeartbeatCode.INTERNAL_ERROR
                 self.msg.details = "Watchdog: the duration between two updates was too long, error"
                 self.msg.header.stamp = rospy.Time.now()
             self.heartbeat_publisher.publish(self.msg)
             self.mutex.release()
             self.rate.sleep()
-
 
     def shutdown(self):
         self.stop_heartbeat = True
